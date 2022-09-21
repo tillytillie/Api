262c3d79798644b991ea45d955a3f6528df26dc5
tillytillie <tillytillie1@gmail.com>
2022-09-21 07:08:36 -0400

    form

---
+++ RapidAPi/js/script.js
@@ -0,0 +1,82 @@
+// 'use strict'
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '79184f2c7bmsh609fbeb4ab89ed4p14e8a4jsn4ca0e725e1d5',
+// 		'X-RapidAPI-Host': 'bloomberg-market-and-financial-news.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete?query=%3CREQUIRED%3E', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+    var url = 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete?query=%3CREQUIRED%3E'
+
+    var apiKey = '79184f2c7bmsh609fbeb4ab89ed4p14e8a4jsn4ca0e725e1d5'
+
+     var host = 'bloomberg-market-and-financial-news.p.rapidapi.com'
+     
+    //  function RapidAPI(){
+
+     fetch(url, {
+        method: 'GET',
+        headers:{
+            'X-RapidAPI-Key': `${apiKey}`,
+            'X-RapidAPI-Host': `${host}`
+        }
+    
+      })
+
+      .then((response) => {
+        if(response.ok){
+            response.json()
+            .then((response) => console.log(response.news))
+            // .then((response)=> console.log(response.news))
+
+            let dNews = response.news 
+            
+
+            if(dNews.length > 0) {
+                var displayNew = ""
+            }
+
+
+            // dNews.forEach(mobile => {
+            //     for (let key in mobile) {
+            //         console.log(`${key}: ${mobile[key]}`);
+            //     }
+            // });
+
+
+            // for loops 
+            dNews.forEach((dNews => {
+                displayNew += "<tr>"
+                displayNew += `<td>${dNews.card} </td>`
+                displayNew += `<td>${dNews.date} </td>`
+                displayNew += `<td>${dNews.id} </td>`
+                displayNew += `<td>${dNews.title} </td>`; "</tr>"
+                  
+            })
+
+
+            
+       )}
+
+        
+      document.getElementById('news').innerHTML = displayNew
+    //   console.log(dNews.card)
+
+
+      })
+      
+
+      .catch(err => console.log(err))
+    
+      // }
+
+    
+    // RapidAPI()
--- js/js.js
+++ js/js.js
@@ -1,40 +1,66 @@
-// var coinUrl = 'https://api.coinranking.com/v2/coins';
-//  var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-//  var ApiKey = 'coinrankinga60119611687bbbf68c115c3e6b329dec954189092f973df';
+var coinUrl = 'https://api.coinranking.com/v2/coins';
+ var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+ var ApiKey = 'coinrankinga60119611687bbbf68c115c3e6b329dec954189092f973df';
 
+fetch(coinUrl, {
+    method:'GET',
+    headers:{ 
+        'Content-Type': 'Application/json',
+        'X-access-token': `${ApiKey}`,
+          'Origin': 'https://cryptopricesapi.netlify.app',
+          'Access-Control-Allow-origin': 'https://cryptopricesapi.netlify.app'
+             
+        }
+    } 
+)
+        // .then((response) => {
+        //     if(response.ok) {
+        //         response.json()
+                
+        //     .then((data) => {
+        //         console.log(data)
+        //     })
+        //   }
+        //         else throw new Errors(console.log(err))
+        //  }))
+
+    
 
 // "public_key": YOUR_PUBLIC_KEY, //string
 // "private_key": YOUR_PRIVATE_KEY, //string
 // "phone": Data recipient phone number, //string
 // "data_id": DATA_ID, //integer... See table below for list of all Data IDs
-var url = ' https://speedydata.com.ng/api/data-order/create'
+// var url = ' https://speedydata.com.ng/api/data-order/create'
 
 
- fetch(url), {
-    method:'POST',
-    headers: {
-        'Content-Type': 'Application/json',
-        // 'X-access-token': `${ApiKey}`,
-        "public_key": 'pub_mO3NrXc4QvvWoqmiIpR3',
-        "private_key": 'pri_2IcdliZYbOET1yLljWzfKyueHcXctH',
-        "phone": '08035583068',
-        "data_id": 1,
-        //  'Origin': 'https://cryptopricesapi.netlify.app',
+//  fetch(url, {
+//     method:'POST',
+//     // mode: 'no-cors', // no-cors, *cors, same-origin
+//     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     // credentials: 'same-origin', // include, *same-origin, omit
+//     headers: {
+//         'Content-Type': 'Application/json',
+//         // 'X-access-token': `${ApiKey}`,
+//         "public_key": 'pub_mO3NrXc4QvvWoqmiIpR3',
+//         "private_key": 'pri_2IcdliZYbOET1yLljWzfKyueHcXctH',
+//         "phone": '08035583068',
+//         "data_id": 1,
+//         //  'Origin': 'https://cryptopricesapi.netlify.app',
 
-        'Access-Control-Allow-origin': 'https://cryptopricesapi.netlify.app'
+//         'Access-Control-Allow-origin': '*'
          
-    } 
- }
- .then((response) => {
-    if(response.ok) {
-        response.json()
+//     } 
+//  }
+//  .then((response) => {
+//     if(response.ok) {
+//         response.json()
         
-    .then((json) => {
-        console.log(json)
-    })
-}
-        else throw new Errors(console.log(err))
- })
+//     .then((json) => {
+//         console.log(json)
+//     })
+// }
+//         else throw new Errors(console.log(err))
+//  }))
  
 
 
