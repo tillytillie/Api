e9712d148a848aed4a74819a9cf859ce23ea8a2d
tillytillie <tillytillie1@gmail.com>
2022-09-21 07:30:22 -0400

    form

--- RapidAPi/js/script.js
+++ RapidAPi/js/script.js
@@ -14,35 +14,35 @@
 // 	.catch(err => console.error(err));
 
 
-    var url = 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete?query=%3CREQUIRED%3E'
+    // var url = 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete?query=%3CREQUIRED%3E'
 
-    var apiKey = '79184f2c7bmsh609fbeb4ab89ed4p14e8a4jsn4ca0e725e1d5'
+    // var apiKey = '79184f2c7bmsh609fbeb4ab89ed4p14e8a4jsn4ca0e725e1d5'
 
-     var host = 'bloomberg-market-and-financial-news.p.rapidapi.com'
+    //  var host = 'bloomberg-market-and-financial-news.p.rapidapi.com'
      
-    //  function RapidAPI(){
+    // //  function RapidAPI(){
 
-     fetch(url, {
-        method: 'GET',
-        headers:{
-            'X-RapidAPI-Key': `${apiKey}`,
-            'X-RapidAPI-Host': `${host}`
-        }
+    //  fetch(url, {
+    //     method: 'GET',
+    //     headers:{
+    //         'X-RapidAPI-Key': `${apiKey}`,
+    //         'X-RapidAPI-Host': `${host}`
+    //     }
     
-      })
+    //   })
 
-      .then((response) => {
-        if(response.ok){
-            response.json()
-            .then((response) => console.log(response.news))
-            // .then((response)=> console.log(response.news))
+    //   .then((response) => {
+    //     if(response.ok){
+    //         response.json()
+    //         .then((response) => console.log(response.news))
+    //         // .then((response)=> console.log(response.news))
 
-            let dNews = response.news 
+    //         let dNews = response.news 
             
 
-            if(dNews.length > 0) {
-                var displayNew = ""
-            }
+    //         if(dNews.length > 0) {
+    //             var displayNew = ""
+    //         }
 
 
             // dNews.forEach(mobile => {
@@ -53,28 +53,28 @@
 
 
             // for loops 
-            dNews.forEach((dNews => {
-                displayNew += "<tr>"
-                displayNew += `<td>${dNews.card} </td>`
-                displayNew += `<td>${dNews.date} </td>`
-                displayNew += `<td>${dNews.id} </td>`
-                displayNew += `<td>${dNews.title} </td>`; "</tr>"
+    //         dNews.forEach((dNews => {
+    //             displayNew += "<tr>"
+    //             displayNew += `<td>${dNews.card} </td>`
+    //             displayNew += `<td>${dNews.date} </td>`
+    //             displayNew += `<td>${dNews.id} </td>`
+    //             displayNew += `<td>${dNews.title} </td>`; "</tr>"
                   
-            })
+    //         })
 
 
             
-       )}
+    //    )}
 
         
-      document.getElementById('news').innerHTML = displayNew
-    //   console.log(dNews.card)
+    //   document.getElementById('news').innerHTML = displayNew
+    // //   console.log(dNews.card)
 
 
-      })
+    //   })
       
 
-      .catch(err => console.log(err))
+    //   .catch(err => console.log(err))
     
       // }
 
